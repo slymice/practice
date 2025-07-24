403c6ec7ba6e583684888c47059356d800e2d7d6
1v@n <slymice387@gmail.com>
2025-07-25 05:07:03 +0800

    configured config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,30 +7,20 @@ import pluginSecurity from "eslint-plugin-security";
 import pluginNoUnsanitized from "eslint-plugin-no-unsanitized";
 
 export default defineConfig([
+  { ignores: ["assets/js/bootstrap*.js", "vendor/**"] },
   {
     files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
     plugins: {
+      js,
       security: pluginSecurity,
-      "no-unsanitized": pluginNoUnsanitized,
-    },
-    extends: [
-      "plugin:js/recommended",         // from @eslint/js plugin
-      "plugin:security/recommended",   // recommended rules for eslint-plugin-security
-      "plugin:no-unsanitized/DOM",     // recommended rules for no-unsanitized
-    ],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        // You can add more globals here if needed
-      },
-      ecmaVersion: 2024,                // latest ECMAScript version
-      sourceType: "module",
+      "no-unsanitized": pluginNoUnsanitized
     },
     rules: {
       ...js.configs.recommended.rules,
       "security/detect-eval-with-expression": "error",
       "no-unsanitized/method": "warn",
       "no-unsanitized/property": "warn"
-    },
-  },
+    }
+  }
 ]);
